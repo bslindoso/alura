d1d9ce9debe5f4d4fdbeb4fb183ea0aff94d9e7e
Bruno Lindoso <48108317+bslindoso@users.noreply.github.com>
2025-02-22 20:43:58 -0300

    criando adotante

---
+++ formacao_nestjs/01_typescript_contrucao_de_uma_api_com_tipagem_segura/src/repositories/AdotanteRepository.ts
@@ -0,0 +1,14 @@
+import { Repository } from "typeorm";
+import AdotanteEntity from "../entities/AdotanteEntity";
+import InterfaceAdotanteRepository from "./interfaces/InterfaceAdotanteRepository";
+
+export class AdotanteRepository implements InterfaceAdotanteRepository {
+  private repository: Repository<AdotanteEntity>;
+
+  constructor(respository: Repository<AdotanteEntity>) {
+    this.repository = respository;
+  }
+  async criaAdotante(adotante: AdotanteEntity): Promise<void> {
+    await this.repository.save(adotante);
+  }
+}
